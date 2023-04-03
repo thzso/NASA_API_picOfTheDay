10798f21afd3b740be5d8f536e6841b7492e01c5
unknown <thuranszkyzso@gmail.com>
2023-04-03 15:31:22 +0200

    remove catch

--- netlify/functions/getPictureOfTheDay.js
+++ netlify/functions/getPictureOfTheDay.js
@@ -15,32 +15,32 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(res.data),
     };
   } catch (err) {
-    const message = await err.response.data.msg;
+    // const message = await err.response.data.msg;
 
-    if (message.startsWith("Date must be between")) {
-      const yesterdayObj = ((d) => new Date(d.setDate(d.getDate() - 1)))(
-        new Date()
-      );
-      let day = yesterdayObj.getDate();
-      if (day < 10) {
-        day = `0${day}`;
-      }
-      const month = yesterdayObj.getMonth() + 1;
-      const year = yesterdayObj.getFullYear();
-      const yesterday = `${year}-0${month}-${day}`;
+    // if (message.startsWith("Date must be between")) {
+    //   const yesterdayObj = ((d) => new Date(d.setDate(d.getDate() - 1)))(
+    //     new Date()
+    //   );
+    //   let day = yesterdayObj.getDate();
+    //   if (day < 10) {
+    //     day = `0${day}`;
+    //   }
+    //   const month = yesterdayObj.getMonth() + 1;
+    //   const year = yesterdayObj.getFullYear();
+    //   const yesterday = `${year}-0${month}-${day}`;
 
-      const res = await axios(
-        `https://api.nasa.gov/planetary/apod?date=${yesterday}&api_key=${process.env.API_KEY}`
-      );
-      return {
-        statusCode: 200,
-        body: JSON.stringify(res.data),
-      };
-    } else {
+    //   const res = await axios(
+    //     `https://api.nasa.gov/planetary/apod?date=${yesterday}&api_key=${process.env.API_KEY}`
+    //   );
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify(res.data),
+    //   };
+    // } else {
       return {
         statusCode: 500,
         body: JSON.stringify({ err }),
       };
     }
-  }
+  
 };
